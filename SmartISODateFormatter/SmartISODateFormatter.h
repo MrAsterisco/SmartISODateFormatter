ac70db3357a6d68aef1e228584b30b532de9a0ce
Alessio Moiso <a.moiso@pillohealth.com>
2019-03-28 14:36:01 +0100

    Move to framework to support Carthage

---
+++ SmartISODateFormatter/SmartISODateFormatter.h
@@ -0,0 +1,19 @@
+//
+//  SmartISODateFormatter.h
+//  SmartISODateFormatter
+//
+//  Created by Alessio Moiso on 28/03/2019.
+//  Copyright © 2019 MrAsterisco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SmartISODateFormatter.
+FOUNDATION_EXPORT double SmartISODateFormatterVersionNumber;
+
+//! Project version string for SmartISODateFormatter.
+FOUNDATION_EXPORT const unsigned char SmartISODateFormatterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartISODateFormatter/PublicHeader.h>
+
+
