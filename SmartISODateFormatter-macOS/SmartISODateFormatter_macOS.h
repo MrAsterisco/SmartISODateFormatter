1fcced15806ebf194e57a3d60ea5a496ce65797e
Alessio Moiso <a.moiso@pillohealth.com>
2019-03-29 15:01:43 +0100

    Add support for macOS, watchOS and tvOS

---
+++ SmartISODateFormatter-macOS/SmartISODateFormatter_macOS.h
@@ -0,0 +1,19 @@
+//
+//  SmartISODateFormatter_macOS.h
+//  SmartISODateFormatter-macOS
+//
+//  Created by Alessio Moiso on 29/03/2019.
+//  Copyright © 2019 MrAsterisco. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SmartISODateFormatter_macOS.
+FOUNDATION_EXPORT double SmartISODateFormatter_macOSVersionNumber;
+
+//! Project version string for SmartISODateFormatter_macOS.
+FOUNDATION_EXPORT const unsigned char SmartISODateFormatter_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartISODateFormatter_macOS/PublicHeader.h>
+
+
---
+++ SmartISODateFormatter-watchOS/SmartISODateFormatter_watchOS.h
@@ -0,0 +1,19 @@
+//
+//  SmartISODateFormatter_watchOS.h
+//  SmartISODateFormatter-watchOS
+//
+//  Created by Alessio Moiso on 29/03/2019.
+//  Copyright © 2019 MrAsterisco. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for SmartISODateFormatter_watchOS.
+FOUNDATION_EXPORT double SmartISODateFormatter_watchOSVersionNumber;
+
+//! Project version string for SmartISODateFormatter_watchOS.
+FOUNDATION_EXPORT const unsigned char SmartISODateFormatter_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmartISODateFormatter_watchOS/PublicHeader.h>
+
+
